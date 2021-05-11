810cb22b0fc3a369f21b3aeadbfd45ebdb58d37c
evolvewd <vincenzo.bevivino@gmail.com>
2021-05-11 22:21:36 +0200

    dir changes

---
+++ src/components/Viewers.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+const Viewers = (props) => {
+  return (
+    <Container>
+      <Wrap>
+        <img src="/images/viewers-disney.png" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-disney.png" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-disney.png" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-disney.png" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-disney.png" alt="" />
+      </Wrap>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  margin-top: 30px;
+  padding: 30px 0px 26px;
+  display: grid;
+  grid-gap: 25px;
+  gap: 25px;
+  grid-template-columns: repeat(5, minmax(0, 1fr));
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+  }
+`;
+
+const Wrap = styled.div`
+  padding-top: 56px;
+`;
+
+export default Viewers;
